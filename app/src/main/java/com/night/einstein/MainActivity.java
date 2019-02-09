d932dcd1949c55b14b872f0fc055fb0f9e7895af
Impeterlewis <37004313+Impeterlewis@users.noreply.github.com>
2019-02-09 00:36:16 -0500

    Change activity on btn1 click | LinearLayout to FrameLayout | Manifest Activity Fix

--- app/src/main/java/com/night/einstein/MainActivity.java
+++ app/src/main/java/com/night/einstein/MainActivity.java
@@ -1,12 +1,30 @@
 package com.night.einstein;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, YoutubeEinsteinPlayer.class);
+                    startActivity(intent);
+            }
+        });
     }
 }
